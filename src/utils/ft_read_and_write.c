df5968828d1ab6abb99c29540e390d16301fc4d0
Junior <junior@Juniors-MacBook-Pro.local>
2023-01-19 23:59:39 +0300

    0.8

--- src/utils/ft_read_and_write.c
+++ src/utils/ft_read_and_write.c
@@ -14,6 +14,8 @@ void ft_read_and_write(const t_dd *root) {
   while ((c_tmp < root->byte.count) && (rd_byte != 0)) {
     buff[i] = (char *)malloc(sizeof(char) * ((root->byte.bs) + 1));
     rd_byte = read(root->if_fd, buff[i], root->byte.bs);
+    if (rd_byte == -1)
+      ft_exit_sys("read");
     buff[rd_byte + 1] = "\0";
     write(root->of_fd, buff[i], rd_byte);
     c_tmp++;
